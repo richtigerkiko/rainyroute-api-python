281cf251a6b454d967cb4b9ba9d8d0ab5c50744e
Alexander Kiko <elkikos@gmail.com>
2023-04-07 16:23:49 +0200

    tidy up

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,11 @@ from flask_restful import Api
 
 from app.controllers.weatherroute import weatherController
 
+
 def create_app():
     app = Flask(__name__)
-    # api = Api(app)
-    
-    
+
     # register routes
     app.register_blueprint(weatherController)
-    
-    
+
     return app
